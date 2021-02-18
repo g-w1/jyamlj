1a68a2c1227665c8e5d4bf07d12c9ea826ab5581
jacob gw <jacoblevgw@gmail.com>
2021-02-17 21:00:52 -0500

    add cli and update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # jyamlj
 
+Java Yaml Json
+
 Convert from json to yaml/json.
 
 Useful if you dont understand yaml and dont but like json.
@@ -11,4 +13,44 @@ TODO:
 * [x] json renderer
 * [x] yaml renderer
 * [x] tests
-* [ ] publish/cli!
+* [x] publish/cli!
+
+## Usage: 
+```
+jyamlj:
+usage: jyamlj [-j] [-y]
+-j: output json (default)
+-y: output yaml
+pipe the json you want to parse and render to stdin
+```
+
+## Building
+In the root dir do `gradle assembleDist`.
+You will find a `.tar` and a `.zip` in `build/distributions/`
+you can unzip these files and you will find a bin folder with 2 files:
+`jyamlj` and `jyamlj.bat`
+use the jyamlj file in unixy systems and jyamlj.bat on Windows to run jyamlj.
+
+## Example Usage:
+
+### Yaml Output: 
+```
+❯ echo "{\"a\":[1,2,3]}" |  ./jyamlj -y
+a: 	- 1
+	- 2
+	- 	- 3
+	- 	
+```
+
+### Json Output:
+```
+❯ echo "{\"a\":[1,2,3]}" |  ./jyamlj -j
+{
+	"a": [
+		1,
+		2,
+		3
+	]
+}
+```
+
--- src/main/java/jyamlj/Main.java
+++ src/main/java/jyamlj/Main.java
@@ -2,14 +2,41 @@ package jyamlj;
 
 import java.util.ArrayList;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-    	String someJson = "[{\"jeff\":\"test\", \"jacob\":[1,2, 3, {\"a\": 1.1, \"b\":5}, [1,2,3]], \"thing\":[1,5.1,[{}]]}]";
-    	System.out.println("input: " + someJson);
-    	JsonLexer l = new JsonLexer();
-    	ArrayList<jyamlj.JsonLexer.JsonTokenPair> tpl = l.lex(someJson);
-    	ParsedObject o = ParsedObject.parseJsonRoot(tpl);
-    	System.out.println(o.toString(false));
+	public static void main(String[] args) {
+        String help = "jyamlj:\n"+
+        "usage: jyamlj [-j] [-y]\n"+
+        "-j: output json (default)\n"+
+        "-y: output yaml\n"+
+        "pipe the json you want to parse and render to stdin\n";
+        boolean toJson = true;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String someJson = "";
+            while (scanner.hasNextLine()) {
+                someJson += scanner.nextLine();
+            }
+            someJson = someJson.trim();
+            if (someJson.length() == 0) {
+                System.out.println("invalid Json, please enter something to stdin");
+                return;
+            }
+            if (args.length > 1)
+                System.out.println("Invalid amount of arguments. Expected 0 or 1");
+            if (args.length == 1) {
+                if (args[0].trim().equals("-y")) {
+                    toJson = false;
+                }
+            }
+        	JsonLexer l = new JsonLexer();
+        	ArrayList<jyamlj.JsonLexer.JsonTokenPair> tpl = l.lex(someJson);
+        	ParsedObject o = ParsedObject.parseJsonRoot(tpl);
+        	System.out.println(o.toString(toJson));
+        } catch (Exception e) {
+            System.out.println("error: " + e);
+        }
 	}
 }
